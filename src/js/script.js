0b3c9b64335644a3315b04b3b6fe929651fee56f
Maxim <ttoptpet@mail.ru>
2022-07-09 11:32:52 +0300

    third commit/slider created

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider__inner').slick({
+        speed: 1000,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/slider/right.svg"></button>'
+    });
+});
+      
